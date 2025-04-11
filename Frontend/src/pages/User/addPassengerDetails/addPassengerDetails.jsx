0b4be7a3cf487fc9ad8996bf9071e28164506160
Naresh9313 <nareshprajapati9313@gmail.com>
2025-04-11 10:15:21 +0530

    solve passnger details

--- Frontend/src/pages/User/addPassengerDetails/addPassengerDetails.jsx
+++ Frontend/src/pages/User/addPassengerDetails/addPassengerDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import "./addPassengerDetails.css";
+import "./addPassengerDetails.css";
 
 export default function AddPassengerDetails() {
   const navigate = useNavigate();
