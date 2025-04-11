6365bd692ac9598444e485bf28ecee6ea4a5994a
Naresh9313 <nareshprajapati9313@gmail.com>
2025-04-11 10:04:52 +0530

    Add .env to .gitignore

--- Frontend/src/pages/User/addPassengerDetails/addPassengerDetails.jsx
+++ Frontend/src/pages/User/addPassengerDetails/addPassengerDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../../User/addPassengerDetails/addPassengerDetails.css";
+import "./addPassengerDetails.css";
 
 export default function AddPassengerDetails() {
   const navigate = useNavigate();
